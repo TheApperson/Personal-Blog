5b1cd46aee149c645514a0e324d4a4a16f1e78d6
Colin Apperson <capp719@gmail.com>
2023-01-19 23:52:51 -0700

    test for deploy

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ from flask_app import app
 from flask_app.controllers import users
 from flask_app.controllers import posts
 
-if __name__=="__main__":
-    app.run(port=3000, debug=True)
-    print('running')
+"""if __name__=="__main__":
+    app.run(debug=True)
+    print('running')"""
+
+if __name__ == "__main__":
+    app.run()
