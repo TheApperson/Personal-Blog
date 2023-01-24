90ce96a8e8c562543ab25dd383ee60f66184bc44
Colin Apperson <capp719@gmail.com>
2023-01-23 22:51:57 -0700

    added turn counter for guessing game

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -81,27 +81,38 @@ def build_list(x):
 
 @app.route('/guesser', methods=['GET', 'POST'])
 def guesser():
-    build_list(100000)
+    build_list(1000)
     if request.method == 'POST':
         last_guess = 0
+        last_answer = 0
+        remaining_guess = 0
         guess = request.form['guess']
         if 'answer' not in session:
-            session['answer'] = random.randint(1, 100000)
+            session['answer'] = random.randint(1, 1000)
             session['guess_min'] = 1
-            session['guess_max'] = 100000
+            session['guess_max'] = 1000
+            session['turns_left'] = 15
         else:
-            if guess == "":
+            if int(session['turns_left']) == 1:
+                last_answer=session['answer']
+                flash(f"No more guesses.. you lose! :( The answer was {last_answer}", "guess")
+                session.pop('answer', None)
+            elif guess == "":
                 flash("Enter a guess!","guess")
-            elif int(guess) <= int(session['guess_min']) or int(guess) >= int(session['guess_max']):
+            elif int(guess) < int(session['guess_min']) or int(guess) > int(session['guess_max']):
                 flash("Invalid guess","guess")
             elif int(guess) > session['answer']:
                 last_guess = guess
+                session['turns_left'] -= 1
                 session['guess_max'] = guess
             elif int(guess) < session['answer']:
                 last_guess = guess
+                session['turns_left'] -= 1
                 session['guess_min'] = guess
             else:
-                last_guess = guess
+                remaining_guess = session['turns_left']
+                flash(f"You win! With {remaining_guess} guesses left.", "guess")
+                session.pop('answer', None)
         return render_template('guesser.html', answer=session.get('answer'), guess_min=session.get('guess_min'),
-        guess_max=session.get('guess_max'), guess = last_guess)
+        guess_max=session.get('guess_max'), guess = last_guess, turns_left=session.get('turns_left'), last_answer=last_answer, remaining_guess=remaining_guess)
     return render_template('guesser.html')
