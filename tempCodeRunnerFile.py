326c4dcb8c71fa1b856491083bde18bffda13250
Colin Apperson <capp719@gmail.com>
2023-01-31 17:21:55 -0700

    temporary server file

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+from flask_app import app
+from flask_app.controllers import users
+from flask_app.controllers import posts
+
+if __name__=="__main__":
+    app.run(port=3308,debug=True)
+    print('running')
